499438360b1e344385c0616ced457cdb85a4f28d
JuanDols <juandols@paucasesnovescifp.cat>
2025-01-28 15:59:53 +0100

    Update people.php

--- people.php
+++ people.php
@@ -11,7 +11,7 @@ require 'db_connection.php';
 </head>
 <body>
     <p>Add a New Follower</p>
-    <form method="POST" action="follow_user.php">
+    <form method="POST" action="people.php">
     <label for="seguir">Nombre de usuario a seguir:</label>
     <input type="text" id="seguir" name="seguir" required>
     <button type="submit">Add follower</button>
@@ -24,7 +24,7 @@ require 'db_connection.php';
     // Verificar si se recibió el nombre del usuario a seguir desde un formulario
     if (isset($_POST['seguir'])) {
         $username_to_follow = $_POST['seguir'];
-        $follower_id = 1; //$_SESSION['user_id'];  // ID del usuario logueado
+        $follower_id = $_SESSION['logged_id']; //$_SESSION['user_id'];  // ID del usuario logueado
     
         // Obtiene el ID del usuario a seguir y verifica si existe
         $sql = "SELECT id FROM users WHERE username = '$username_to_follow'";
@@ -60,15 +60,14 @@ require 'db_connection.php';
     <?php
     //TODO @joan hacer estructura - manage people
     //para seguir a alguien nuevo, insertar en tabla followers. follower_id es el usuario actual, following_id sera el id del usuario que se va a seguir
-    $sql = "SELECT u.id, u.username FROM followers f JOIN users u ON f.following_id = u.id 
-        WHERE f.follower_id = 1 ORDER BY u.username DESC"; //1 deberia ser la id de la sesion
+        $sql = "SELECT u.id, u.username FROM followers f JOIN users u ON f.follower_id = u.id 
+        WHERE f.following_id = ".$_SESSION['logged_id']." ORDER BY u.username DESC"; //1 deberia ser la id de la sesion
         $result = execQuery(getDbConnection(), $sql);
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                 echo "<li>" . htmlspecialchars($row["username"]) . "</li>";
             }
         }
-
     ?>
     </ul>
 
@@ -77,24 +76,17 @@ require 'db_connection.php';
     <?php
     //TODO @joan hacer estructura - manage people
     //para seguir a alguien nuevo, insertar en tabla followers. follower_id es el usuario actual, following_id sera el id del usuario que se va a seguir
-    $sql = "SELECT u.id, u.username FROM followers f JOIN users u ON f.follower_id = u.id 
-        WHERE f.following_id = 1 ORDER BY u.username DESC"; //1 deberia ser la id de la sesion
-        $result = execQuery(getDbConnection(), $sql);
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<li>" . htmlspecialchars($row["username"]) . "</li>";
-            }
+    $sql = "SELECT u.id, u.username FROM followers f JOIN users u ON f.following_id = u.id 
+    WHERE f.follower_id = ".$_SESSION['logged_id']." ORDER BY u.username DESC"; //1 deberia ser la id de la sesion
+    $result = execQuery(getDbConnection(), $sql);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<li>" . htmlspecialchars($row["username"]) . "</li>";
         }
+    }
 
     ?>
     </ul>
 </body>
 </html>
 
-// $sql = 'SELECT ph.user_id, us.username FROM instagram_clone.photos ph JOIN users us ON ph.user_id = us.id WHERE user_id = '.$_SESSION["logged_id"].' ORDER BY created_at DESC';
-//        $result = execQuery(getDbConnection(), $sql);
-//        if (mysqli_num_rows($result) > 0) {
-//            while ($row = mysqli_fetch_assoc($result)) {
-//                //recorrer resultado de la consulta con $row["nombre columna"]
-//            }
-//        }
